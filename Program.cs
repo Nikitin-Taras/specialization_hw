85892b899b43f22b7c2950a963613e67e5396960
Tarik <nikitin-taras@yandex.ru>
2023-03-04 19:41:47 +0200

    solved the task

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+string[] arr1 = new string[7] {"22", "ok", "no", "333", "hello", "4444", "-5"};
+string[] arr2 = new string[arr1.Length];
+void ResArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("\b\b\b]");
+}
+ResArray(arr1, arr2);
+PrintArray(arr2);
